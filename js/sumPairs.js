29a6c14b6228ff081aa5d6f3607dc810d2149125
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-01 20:18:10 -0400

    js solution

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,20 @@
-exports.sumPairs = function() {
+exports.sumPairs = function(num_list, num) {
+    ansArr = []
+
+    for(i = 0; i < num_list.length; i++){
+        for(j = 0; j < num_list.length; j++){
+            if((num_list[i] + num_list[j] == num) && (i < j)){
+                ansArr.push([num_list[i], num_list[j]])
+            }
+        }
+    }
+    if (ansArr.length == 0){
+        return 'unable to find pairs'
+    }
+    
+    return ansArr
 
 };
+console.log(exports.sumPairs([1,2,3,4,5], 9))
+console.log(exports.sumPairs([1,2,3,4,5], 7))
+console.log(exports.sumPairs([3, 1, 5, 8, 2], 27))
